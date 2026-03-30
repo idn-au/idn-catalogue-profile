a8822ee7df67c72306e7d72ce5764dc3678c5ec3
Nicholas Car <nick@kurrawong.ai>
2026-03-30 13:16:12 +1000

    add validator testing files

--- resources/tests/test_all.py
+++ resources/tests/test_all.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from kurra.shacl import validate
 
-for f in sorted(Path(__file__).parent.glob("*.ttl")):
-    if "invalid" in str(f):
-        print(f"invalid: {f}")
-    else:
-        print(f"valid: {f}")
+def test_all():
+    for f in sorted(Path(__file__).parent.glob("*.ttl")):
+        v = validate(f, Path(__file__).parent.parent / "validator.ttl")
+
+        if "invalid" in str(f):
+            assert not v[0], f"File {f} should have failed validation but did not"
+        else:
+            assert v[0], f"File {f} failed validation but should not"
